e866d43cc0856520a3f76efa120334d22c29ffd9
Shulelk <as.fss.ss.0223@gmail.com>
2023-03-04 09:18:08 +0800

    second

---
+++ js/title.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '╭(°A°`)╮ 你去哪了?!';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 你肥来啦!';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
